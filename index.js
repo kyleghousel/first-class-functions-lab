0e1e6a31c27e14040f0e3c2dc27abab5846676ce
Kyle Housel <kylegranthousel@gmail.com>
2025-05-06 10:03:16 -0400

    Create fareDoubler and fareMultiplier functions

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
-const returnFirstTwoDrivers = (array) => array.slice(2)
+const returnFirstTwoDrivers = (array) => array.slice(0, 2);
 
-const returnLastTwoDrivers = (array) => array.slice(-2)
+const returnLastTwoDrivers = (array) => array.slice(-2);
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const createFareMultiplier = multiplier => fare => multiplier * fare;
+
+const fareDoubler = fare => fare * 2;
